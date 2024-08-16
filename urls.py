4ac637986b8efa2b9beaf7b6a07f9fe44640505d
Guru-prasadrana <guruprasadrana571@gmail.com>
2024-08-16 11:16:32 +0530

    Add files via upload

--- urls.py
+++ urls.py
@@ -1,23 +1,33 @@
+"""
+URL configuration for studentstudyportal project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/5.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
 from django.urls import path
-from . import views
+from django.urls.conf import include
+from dashboard import views as dash_views
+from django.contrib.auth import views as auth_views
+
 urlpatterns = [
-    path("",views.home,name='home'),
-    path('notes/',views.notes,name='notes'),
-    path('delete_notes/<int:pk>',views.delete_note, name="delete-note"),
-    path('notes_detail/<int:pk>',views.NotesDetailView.as_view(), name="notes-detail"),
-    path('homework/',views.homework,name="homework"),
-    path('update_homework/<int:pk>',views.update_homework, name="update-homework"),
-    path('delte_homework/<int:pk>',views.delete_homework, name="delete-homework"),
-    path('youtube',views.youtube, name="youtube"),
-
-    path('todo',views.todo, name="todo"),
-     path('update_todo/<int:pk>',views.update_todo, name="update-todo"),
-     path('delte_todo/<int:pk>',views.delete_todo, name="delete-todo"),
-
-     path('books',views.books, name="books"),
-     path('dictionary',views.dictionary, name="dictionary"),
-     path('wiki',views.wiki, name="wiki"),
-     path('conversion',views.conversion, name="conversion"),
-
-
+    path('admin/', admin.site.urls),
+    path('',include('dashboard.urls')),
+    path('register/',dash_views.register,name='register'),
+    path('login/',auth_views.LoginView.as_view(template_name="dashboard/login.html"),name='login'),
+    #path('logout/',auth_views.LogoutView.as_view(template_name="dashboard/logout.html"),name='logout'),
+    path('logout/', dash_views.custom_logout, name='logout'),
+    path('profile/',dash_views.profile,name='profile'),
+     
+    
 ]
